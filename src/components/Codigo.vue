974f37842b21d38c4270a080c2b5a13651c9e887
tedesco8 <tedesco8@hotmail.com>
2021-01-11 19:38:02 -0300

    programando funcion y agregando cordova

---
+++ src/components/Codigo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="principal">
+      <h1>Código</h1>
+    </div>
+    <div class="boton">
+      <input type="button" value="Escanear código" @click="scan()" />
+    </div>
+  </div>
+</template>
+
+<script>
+import cordova from "vue-cli-plugin-cordova";
+export default {
+  name: "Codigo",
+  methods: {
+    scan() {
+      cordova.plugins.barcodeScanner.scan(
+        function(result) {
+            console.log(result)
+            console.log(result.text)
+        },
+        function(err) {
+          console.log(err);
+        }
+      );
+    },
+  },
+};
+</script>
+
+<style></style>
