4a2f8253eb94faf5956d9e8fb8ae711c879ec4da
tedesco8 <tedesco8@hotmail.com>
2021-01-12 18:05:43 -0300

    codificando data en CODE39

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link></router-link>
+      <router-link to="/"></router-link>
     </div>
     <router-view/>
   </div>
--- src/components/Codigo.vue
+++ src/components/Codigo.vue
@@ -3,29 +3,49 @@
     <div class="principal">
       <h1>Código</h1>
     </div>
-    <div class="boton">
-      <input type="button" value="Escanear código" @click="scan()" />
-    </div>
+    <input v-model="barcodeValue" /><br />
+    <barcode :value="barcodeValue" :tag="tag" :options="options" >
+      Show this if the rendering fails.
+    </barcode>
   </div>
 </template>
 
 <script>
-import cordova from "vue-cli-plugin-cordova";
+import VueBarcode from "vue-barcode";
 export default {
   name: "Codigo",
-  methods: {
-    scan() {
-      cordova.plugins.barcodeScanner.scan(
-        function(result) {
-            console.log(result)
-            console.log(result.text)
-        },
-        function(err) {
-          console.log(err);
-        }
-      );
-    },
+  data() {
+    return {
+      barcodeValue: "test",
+      tag: "svg",
+      options: {
+        lineColor: "#ff7069",
+        fontSize: 32,
+        font: "Courier",
+        width: 3,
+        height: 60,
+        marginBottom: 60,
+        format: "CODE39",
+        background: "#ccffff",
+      },
+    };
   },
+  components: {
+    barcode: VueBarcode,
+  },
+  // methods: {
+  //   scan() {
+  //     this.$cordova.plugins.barcodeScanner.scan(
+  //       function(result) {
+  //         console.log(result);
+  //         console.log(result.text);
+  //       },
+  //       function(err) {
+  //         console.log(err);
+  //       }
+  //     );
+  //   },
+  // },
 };
 </script>
 
