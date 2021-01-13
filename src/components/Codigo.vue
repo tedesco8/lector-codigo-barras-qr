ed6cb893dc04fe2a28bf45b688a6d981cea6bbd2
tedesco8 <tedesco8@hotmail.com>
2021-01-12 21:28:51 -0300

    encryptando valor

--- src/components/Codigo.vue
+++ src/components/Codigo.vue
@@ -4,26 +4,29 @@
       <h1>Código</h1>
     </div>
     <input v-model="barcodeValue" /><br />
-    <barcode :value="barcodeValue" :tag="tag" :options="options" >
+    <barcode :value="newBarcodevalue" :tag="tag" :options="options">
       Show this if the rendering fails.
     </barcode>
   </div>
 </template>
 
 <script>
+import bcrypt from 'bcryptjs'
 import VueBarcode from "vue-barcode";
 export default {
   name: "Codigo",
   data() {
     return {
       barcodeValue: "test",
+      newBarcodevalue: "",
       tag: "svg",
+      rounds: 10,
       options: {
         lineColor: "#ff7069",
         fontSize: 32,
         font: "Courier",
-        width: 3,
-        height: 60,
+        width: 1,
+        height: 100,
         marginBottom: 60,
         format: "CODE39",
         background: "#ccffff",
@@ -33,19 +36,31 @@ export default {
   components: {
     barcode: VueBarcode,
   },
-  // methods: {
-  //   scan() {
-  //     this.$cordova.plugins.barcodeScanner.scan(
-  //       function(result) {
-  //         console.log(result);
-  //         console.log(result.text);
-  //       },
-  //       function(err) {
-  //         console.log(err);
-  //       }
-  //     );
-  //   },
-  // },
+  watch: {
+    barcodeValue(nuevoValor, valorAnterior){
+      console.log("El nombre pasó de ser %s a %s", valorAnterior, nuevoValor);
+      this.hashValue(nuevoValor)
+    }
+  },
+  methods: {
+    async hashValue() {
+      const hash = await bcrypt.hash(this.barcodeValue, this.rounds);
+      console.log(hash);
+      console.log(await bcrypt.compare(this.barcodeValue, hash));
+      this.newBarcodevalue = hash
+    },
+    // scan() {
+    //   cordova.plugins.barcodeScanner.scan(
+    //     function(result) {
+    //       console.log(result);
+    //       console.log(result.text);
+    //     },
+    //     function(err) {
+    //       console.log(err);
+    //     }
+    //   );
+    // },
+  },
 };
 </script>
 
